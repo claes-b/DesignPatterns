c74fa23760297e71f700c485cd875a4b0de25fe2
Claes Barthelson <claes.barthelson@gmail.com>
2021-12-11 12:08:35 +0100

    adapter

---
+++ Adapter/Program.cs
@@ -0,0 +1,36 @@
+﻿public interface IAdatper
+{
+    string CompatibleMethod();
+}
+
+class SpecificClass
+{
+    public string SomeSpecificMethod()
+    {
+        return "Some specific stuff";
+    }
+}
+
+class Adapter : IAdatper
+{
+    private readonly SpecificClass _adaptee;
+
+    public Adapter(SpecificClass adaptee)
+    {
+        _adaptee = adaptee;
+    }
+
+    public string CompatibleMethod()
+    {
+        return _adaptee.SomeSpecificMethod();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IAdatper target = new Adapter(new SpecificClass());
+        Console.WriteLine(target.CompatibleMethod());
+    }
+}
